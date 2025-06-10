39c9d6f05c79abaf619edd653f573e88cfbe027e
kundan <kundan8789@gmail.com>
2025-06-10 18:53:14 +0530

    add authenticatrion

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
 
 
 
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { getCurrentUser } from '../controllers/user.controllers.js'
+
+const userRouter=express.Router()
+
+
+userRouter.post("/current",getCurrentUser)
+
+export default userRouter
